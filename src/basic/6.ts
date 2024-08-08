05b8d58dd98362acfc9d33c11997747c0abb1f72
Mark-goga <markgala06@gmail.com>
2024-08-08 15:27:48 +0300

    finish basic and generics

---
+++ src/basic/6.ts
@@ -0,0 +1,27 @@
+interface City  {
+  city: string,
+  country: string
+}
+
+interface User {
+  name: string,
+  age: number,
+  email: string,
+  address?: City
+}
+
+const mango: User = {
+ name: 'Mango',
+ age: 30,
+ email: 'john@example.com',
+ address: {
+  city: 'New York',
+  country: 'USA'
+ }
+};
+
+const poly: User = {
+ name: 'Mango',
+ age: 30,
+ email: 'john@example.com'
+};
